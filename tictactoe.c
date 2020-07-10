9855a3750a8ad909e72e1a359902d8a7bb42d5e6
Sam Weinstein <raeweinst@gmail.com>
2020-07-10 12:04:54 -0400

    recursion!

--- tictactoe.c
+++ tictactoe.c
@@ -3,17 +3,19 @@
 #include <math.h>
 #include <string.h>
 
-const int size;
-const int dimensions;
 const char chars[3]={' ', 'X', 'O'};
+int size;
+int dimensions;
+
+
 enum Move {
-    None,
-    X,
-    O
+	None,
+	X,
+	O
 };
 
-enum Move * pos(int *pos, enum Move * board){
-	for(int i=0; i<dimensions; i++){
+enum Move * pos(int *pos, enum Move * board, int d){
+	for(int i=0; i<d; i++){
 		board += pos[i]*(int)pow(size,i+1);
 	}
 	return board;
@@ -21,6 +23,10 @@ enum Move * pos(int *pos, enum Move * board){
 
 char * draw(enum Move * board, int d){ //MUST FREE RESULT
 	char * result = malloc(3);
+	int coord[d];
+	for(int i=0; i<d; i++){//initialize coord array
+		coord[i]=0;
+	}
 	switch(d%2){
 		case 0: //even dimension, vertical
 			if(d==0){
@@ -34,7 +40,8 @@ char * draw(enum Move * board, int d){ //MUST FREE RESULT
 			break;
 		case 1: //odd dimension, horizontal
 			for(int i=0; i<size; i++){
-				char *unit = draw//ah fuckkkkk this is so convoluted
+				coord[d-1]=i;
+				char *unit = draw(pos(coord, board, d), d-1);
 				if(i!=size-1){
 					
 				} else {
