f890d77d7a6d35edaef3146f5ee63af6c6e23426
samuel-weinstein-cs <raeweinst@gmail.com>
2020-07-10 23:04:08 -0400

    string formatting is really hard

--- tictactoe.c
+++ tictactoe.c
@@ -27,30 +27,28 @@ char * draw(enum Move * board, int d){ //MUST FREE RESULT
 	for(int i=0; i<d; i++){//initialize coord array
 		coord[i]=0;
 	}
-	switch(d%2){
-		case 0: //even dimension, vertical
-			if(d==0){
-				sprintf(result, " %c", chars[*board]);
-				return result;
-			} else {
-				for(int i=0; i<size; i++){
-					// do stuff
-				}
+	for(int i=0; i<size; i++){
+		coord[d-1]=i;
+		char *unit = draw(pos(coord, board, d), d-1);
+		if(d==0){
+			sprintf(result, " %c", chars[*board]);
+			return result;
+		} else {
+			switch(d%2){
+				case 0: //even dimension, vertical
+					break;
+				case 1: //odd dimension, horizontal
+						result = realloc(result, strlen(result)+strlen(unit)+3);
+						if(i!=size-1){
+							sprintf(result, "| ");
+						} else {
+							sprintf(result, " ");
+						}
+					break;
 			}
-			break;
-		case 1: //odd dimension, horizontal
-			for(int i=0; i<size; i++){
-				coord[d-1]=i;
-				char *unit = draw(pos(coord, board, d), d-1);
-				if(i!=size-1){
-					
-				} else {
-					
-				}
-			}
-			break;
+		}
 	}
-	return result;	
+	return result;
 }
 
 
@@ -60,8 +58,8 @@ int main() {
 	printf("enter dimensions: ");
 	scanf("%d", &dimensions);
 	enum Move * board = calloc(sizeof(enum Move), (int)pow(size,dimensions));
-	enum Move test = X;
-	char *output = draw(&test, 0);
+	// enum Move test = X;
+	char *output = draw(board, dimensions);
 	printf("%s", output);
 	free(output);//MUST free result from draw
 	return 0;
